c1924bd663a00540e77b8d4a2f0b924d181aaf22
Fernando <fernando93d@gmail.com>
2024-11-22 12:29:41 -0600

    Snar

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,7 +7,7 @@ class CalculatorTest extends TestCase
 {
     public function testAdd()
     {
-        // hooa
+        $password = '123asdfa2';
         $calculator = new Calculator();
         $this->assertEquals(5, $calculator->add(2, 3), "Adding 2 and 3 should return 5");
     }
