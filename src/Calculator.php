7b9fe98bd00e4d11cfbeb2c9e51bd0734487f347
Fernando <fernando93d@gmail.com>
2024-11-30 12:29:20 -0600

    Wip

--- src/Calculator.php
+++ src/Calculator.php
@@ -6,14 +6,6 @@ class Calculator
 {
     public function add($a, $b)
     {
-        $password = 'testroot';
-
-        echo $password;
-
-        if ($a === 42) {
-            exit(23);
-        }
-
         return $a + $b;
     }
 }
